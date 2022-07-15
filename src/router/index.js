1065f36cad0dfe70c7f2e10e701772a83a371eb4
Bravoo Sonja <bravoosonja@gmail.com>
2022-07-16 00:35:25 +1000

    Add route guard for authenticated users

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,21 @@ import Welcome from "../views/Welcome.vue";
 import Chatroom from "../views/Chatroom.vue";
 import { projectAuth } from "../firebase/config";
 
-// auth guard
+// auth guard for non-authenticated users
 const requireAuth = (to, form, next) => {
   let user = projectAuth.currentUser;
-  console.log("Current user in auth guard: ", user);
   if (!user) {
     next({ name: "Welcome" });
+  } else {
+    next();
+  }
+};
+
+// route guard for authenticated users
+const requireNoAuth = (to, from, next) => {
+  let user = projectAuth.currentUser;
+  if (user) {
+    next({ name: "Chatroom" });
   } else {
     next();
   }
@@ -21,6 +30,7 @@ const router = createRouter({
       path: "/",
       name: "Welcome",
       component: Welcome,
+      beforeEnter: requireNoAuth,
     },
     {
       path: "/chatroom",
