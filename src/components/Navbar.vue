133aa3e4fae1ae70abad22788b66c570cd47498e
Bravoo Sonja <bravoosonja@gmail.com>
2022-07-16 10:58:12 +1000

    Add theme toggler

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,29 +6,50 @@
       </p>
       <p class="email">Currently logged in as 📧 {{ user.email }}</p>
     </div>
-    <button @click="handleClick">Logout</button>
+    <div class="buttons">
+      <button @click="toggleDark()" id="toggle-theme">
+        <img
+          v-if="isDark"
+          src="../assets/images/icon-sun.svg"
+          alt="switch to light theme"
+        />
+        <img
+          v-if="!isDark"
+          src="../assets/images/icon-moon.svg"
+          alt="switch to dark theme"
+        />
+      </button>
+
+      <button @click="handleClick">Logout</button>
+    </div>
   </nav>
 </template>
+
 
 <script>
 import useLogout from "../composables/useLogout";
 import getUser from "../composables/getUser";
 
+import { useDark, useToggle } from "@vueuse/core";
+
 export default {
   setup() {
+    const isDark = useDark();
+    const toggleDark = useToggle(isDark);
+
     const { logout, error } = useLogout();
     const { user } = getUser();
-
     const handleClick = async () => {
       await logout();
       if (!error.value) {
         console.log("user logged out");
       }
     };
-    return { handleClick, user };
+    return { handleClick, user, isDark, toggleDark };
   },
 };
 </script>
+
 
 <style>
 nav {
@@ -62,5 +83,19 @@ nav p.email {
 nav button {
   width: 150px;
   color: var(--color-text);
+}
+
+#toggle-theme {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 10px;
+  color: var(--color-text);
+  box-shadow: none;
+}
+
+#toggle-theme::hover {
+  background-color: transparent;
+  color: var(--color-heading);
 }
 </style>
